3bc16605b866fd1be04e66f4719946bd769f0aa3
7L3arash <7l3arash@gmail.com>
2020-09-11 18:19:05 +0430

    2. Move Using Nav Mesh Agent

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] Transform target;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        GetComponent<NavMeshAgent>().destination = target.position;
+    }
+}
