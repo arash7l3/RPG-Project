755827411350e3bb744edb1d6ee073b0c3275a22
7L3arash <7l3arash@gmail.com>
2020-09-13 16:19:11 +0430

    30. Calculating AI Chase Distance

--- Assets/Scripts/Core/FollowCamera.cs
+++ Assets/Scripts/Core/FollowCamera.cs
@@ -1,12 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using RPG.Control;
 
 namespace RPG.Core
 {
     public class FollowCamera : MonoBehaviour
     {
-        [SerializeField] Transform target;
+        // [SerializeField] Transform target;
+
+        Transform target;
+        private void Awake()
+        {
+            target = GameObject.FindWithTag("Player").transform;
+        }
 
         private void LateUpdate()
         {
