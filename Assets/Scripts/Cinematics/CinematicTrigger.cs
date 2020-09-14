67be8be028ff8f0c6a260fddc042c8e39023b742
7L3arash <7l3arash@gmail.com>
2020-09-15 02:24:37 +0430

    47. Player Input In Cutscenes

--- Assets/Scripts/Cinematics/CinematicTrigger.cs
+++ Assets/Scripts/Cinematics/CinematicTrigger.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEditor.Playables;
+﻿using UnityEngine;
 using UnityEngine.Playables;
 
 namespace RPG.Cinematics
