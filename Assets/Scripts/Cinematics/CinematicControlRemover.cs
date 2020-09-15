fb56a964b69749b5e76b30dd8262f7981a787884
7L3arash <7l3arash@gmail.com>
2020-09-15 19:27:02 +0430

    48. Make A Second Scene

--- Assets/Scripts/Cinematics/CinematicControlRemover.cs
+++ Assets/Scripts/Cinematics/CinematicControlRemover.cs
@@ -20,12 +20,18 @@ namespace RPG.Cinematics
         void DisableControl(PlayableDirector pd)
         {
             player.GetComponent<ActionScheduler>().CancelCurrentAction();
-            player.GetComponent<PlayerController>().enabled = false;
+            EnablePlayerController(false);
         }
 
         void EnableControl(PlayableDirector pd)
         {
             player.GetComponent<PlayerController>().enabled = true;
+            EnablePlayerController(true);
+        }
+
+        void EnablePlayerController(bool enable)
+        {
+            player.GetComponent<PlayerController>().enabled = enable;
         }
     }
 }
