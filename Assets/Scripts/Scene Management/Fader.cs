1a8698e642009ca910bcdc457f1af28cc838f3de
7L3arash <7l3arash@gmail.com>
2020-09-16 00:04:12 +0430

    53. Avoiding The Singleton Pattern

--- Assets/Scripts/Scene Management/Fader.cs
+++ Assets/Scripts/Scene Management/Fader.cs
@@ -10,16 +10,6 @@ namespace RPG.SceneManagement
         private void Start()
         {
             canvasGroup = GetComponent<CanvasGroup>();
-
-            StartCoroutine(FadeOutIn());
-        }
-
-        public IEnumerator FadeOutIn()
-        {
-            yield return FadeOut(3f);
-            print("Faded out");
-            yield return FadeIn(1f);
-            print("Faded in");
         }
 
         public IEnumerator FadeOut(float time)
