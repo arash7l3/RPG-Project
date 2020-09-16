35cc7773b89cc52b6203fbbc54bd09d4ba7cfaec
7L3arash <7l3arash@gmail.com>
2020-09-16 17:03:22 +0430

    58. Loading The Saved Scene

--- Assets/Scripts/Scene Management/SavingWrapper.cs
+++ Assets/Scripts/Scene Management/SavingWrapper.cs
@@ -1,11 +1,21 @@
 ﻿using UnityEngine;
 using RPG.Saving;
+using System.Collections;
 
 namespace RPG.SceneManagement
 {
     public class SavingWrapper : MonoBehaviour
     {
         const string defaultSaveFile = "save";
+        [SerializeField] float fadeInTime = 0.2f;
+
+        private IEnumerator Start()
+        {
+            Fader fader = FindObjectOfType<Fader>();
+            fader.FadeOutImmediate();
+            yield return GetComponent<SavingSystem>().LoadLastScene(defaultSaveFile);
+            yield return fader.FadeIn(fadeInTime);
+        }
 
         private void Update()
         {
