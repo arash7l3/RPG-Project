5e6e1c9a0bea0e8e53dfedba4620b2b04c0650cb
7L3arash <7l3arash@gmail.com>
2020-09-19 03:39:03 +0430

    66. Make A Weapon Projectile

---
+++ Assets/Scripts/Combat/Projectile.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+namespace RPG.Combat
+{
+    public class Projectile : MonoBehaviour
+    {
+        [SerializeField] Transform target = null;
+        [SerializeField] float speed = 1f;
+
+        private void Update()
+        {
+            if (target == null) return;
+
+            transform.LookAt(GetAimLocation());
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        }
+
+        private Vector3 GetAimLocation()
+        {
+            CapsuleCollider targetCapsule = target.GetComponent<CapsuleCollider>();
+            if (targetCapsule == null)
+            {
+                return target.position;
+            }
+            return target.position + Vector3.up * targetCapsule.height / 2;
+        }
+    }
+}
