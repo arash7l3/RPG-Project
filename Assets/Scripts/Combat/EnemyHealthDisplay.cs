578ca0dfc7e8dab70dc190afe1df09e12fb8f359
7L3arash <7l3arash@gmail.com>
2020-09-25 20:14:22 +0330

    83. Enemy Health Display

---
+++ Assets/Scripts/Combat/EnemyHealthDisplay.cs
@@ -0,0 +1,30 @@
+﻿using RPG.Combat;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace RPG.Resources
+{
+    public class EnemyHealthDisplay : MonoBehaviour
+    {
+        Fighter fighter;
+
+        private void Awake()
+        {
+            fighter = GameObject.FindWithTag("Player").GetComponent<Fighter>();
+        }
+
+        private void Update()
+        {
+            Health health = fighter.GetTarget();
+            if (health == null)
+            {
+                GetComponent<Text>().text = "N/A";
+            }
+            else
+            {
+                GetComponent<Text>().text = String.Format("{0:0}%", health.GetPercentage());
+            }
+        }
+    }
+}
